5162bd732bb7520f7868b7920233e454a152e316
GABRIELLA RODRIGUES LOPES <GABRIELLA0633@DIGITALCOLLEGE.LOCAL>
2024-08-13 14:59:38 -0300

    adicionando itens

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,58 @@
+const app = require('./app-express.js')
+
+const { Category } = require('../models/Models.js');
+
+app.get('/', (req, res) => {
+    res.send('Olá, mundo')
+})
+
+app.get('/v1/category/search', async (req, res) => {
+    console.log(req.query);
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 12;
+    const fields = req.query.fields
+
+    console.log('fields', fields)
+
+    const category = await Product.findAll({attributes:fields.split(','), limit: limit, offset: limit * (page - 1) })
+    res.send(produtos)
+})
+
+app.get('/v1/category/:id', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.params.id', request.params.id)
+
+    Category.findOne({ where: { id: request.params.id } })
+        .then((result) => res.send(result))
+})
+
+
+app.get('/v1/category/', (request, res) => {
+    console.log('request.url', request.url) // debug
+
+
+    Category.findAll()
+        .then((result) => res.send(result))
+})
+
+app.post('/v1/category', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.body', request.body)
+
+    Category.create(request.body).then((result) => res.status(201).send(result))
+})
+
+
+app.put('/v1/category/:id', (request, res) => {
+    console.log('request.url', request.url) // debug
+    console.log('request.body', request.body)
+    Category.update(request.body, { where: { id: request.params.id } }).then((result) => res.send(result))
+})
+
+app.delete('/v1/category/:id', (request, res) => {
+    console.log('request.url', request.url) // debug
+    Category.destroy({ where: { id: request.params.id } }).then((result) => {
+        res.send('deletei com sucesso essa quantidade de linhas: ' + result)
+    })
+})
